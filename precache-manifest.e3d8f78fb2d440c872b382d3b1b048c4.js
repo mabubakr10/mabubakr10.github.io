a0d1dd6bd344c153f2cf49091935b1c58545b8c9
abubakr <abubakr@sendoso.com>
2025-07-26 00:24:28 +0400

    added web page

--- precache-manifest.e3d8f78fb2d440c872b382d3b1b048c4.js
+++ precache-manifest.e3d8f78fb2d440c872b382d3b1b048c4.js
@@ -5,90 +5,90 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "79946a96df4d04d5e01e",
-    "url": "/static/css/3.df879eac.chunk.css"
+    "url": "/css/3.df879eac.chunk.css"
   },
   {
     "revision": "36b080b5b1dee5a0b6ed",
-    "url": "/static/css/4.e91a4b36.chunk.css"
+    "url": "/css/4.e91a4b36.chunk.css"
   },
   {
     "revision": "04157ae225c2d14feecf",
-    "url": "/static/css/main.1a6005f5.chunk.css"
+    "url": "/css/main.1a6005f5.chunk.css"
   },
   {
     "revision": "e700d59276736dc1c8f0",
-    "url": "/static/js/2.e85f9fa3.chunk.js"
+    "url": "/js/2.e85f9fa3.chunk.js"
   },
   {
     "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
-    "url": "/static/js/2.e85f9fa3.chunk.js.LICENSE.txt"
+    "url": "/js/2.e85f9fa3.chunk.js.LICENSE.txt"
   },
   {
     "revision": "79946a96df4d04d5e01e",
-    "url": "/static/js/3.eb23c5f8.chunk.js"
+    "url": "/js/3.eb23c5f8.chunk.js"
   },
   {
     "revision": "36b080b5b1dee5a0b6ed",
-    "url": "/static/js/4.afe8fb20.chunk.js"
+    "url": "/js/4.afe8fb20.chunk.js"
   },
   {
     "revision": "04157ae225c2d14feecf",
-    "url": "/static/js/main.5be2e54a.chunk.js"
+    "url": "/js/main.5be2e54a.chunk.js"
   },
   {
     "revision": "a8b3f4a340f97053462d",
-    "url": "/static/js/runtime-main.8c1c9fc4.js"
+    "url": "/js/runtime-main.8c1c9fc4.js"
   },
   {
     "revision": "21f233e19402cc4a66866a7f31191f0d",
-    "url": "/static/media/Agustina.21f233e1.woff"
+    "url": "/media/Agustina.21f233e1.woff"
   },
   {
     "revision": "ee6539921d713482b8ccd4d0d23961bb",
-    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+    "url": "/media/Montserrat-Regular.ee653992.ttf"
   },
   {
     "revision": "83d96c06951761bfd093ab408f1639ce",
-    "url": "/static/media/Zameen-Logo.83d96c06.jpg"
+    "url": "/media/Zameen-Logo.83d96c06.jpg"
   },
   {
     "revision": "54db718aaaacf8337d360bbab92a976a",
-    "url": "/static/media/arbisoft-logo.54db718a.jpeg"
+    "url": "/media/arbisoft-logo.54db718a.jpeg"
   },
   {
     "revision": "51cf7192327488da3d2a171280680c16",
-    "url": "/static/media/contactMailDark.51cf7192.svg"
+    "url": "/media/contactMailDark.51cf7192.svg"
   },
   {
     "revision": "59389695208d7454c6607bed51ed4316",
-    "url": "/static/media/developerActivity.59389695.svg"
+    "url": "/media/developerActivity.59389695.svg"
   },
   {
     "revision": "52be31441c8cf8a8a16bd7a228d428bf",
-    "url": "/static/media/manOnTable.52be3144.svg"
+    "url": "/media/manOnTable.52be3144.svg"
   },
   {
     "revision": "829ad0a1deef4d7295adfecdf12df2be",
-    "url": "/static/media/nextuLogo.829ad0a1.webp"
+    "url": "/media/nextuLogo.829ad0a1.webp"
   },
   {
     "revision": "6bdcf54ef3ad4eff0ce1d80425721e1b",
-    "url": "/static/media/nu-logo.6bdcf54e.jpeg"
+    "url": "/media/nu-logo.6bdcf54e.jpeg"
   },
   {
     "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
-    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+    "url": "/media/saayaHealthLogo.25018d29.webp"
   },
   {
     "revision": "3440939881da8339d1597e8d77cdf850",
-    "url": "/static/media/skill.34409398.svg"
+    "url": "/media/skill.34409398.svg"
   },
   {
     "revision": "13780cdd0144cacdef25486e2c2503f8",
-    "url": "/static/media/talksCardBack.13780cdd.svg"
+    "url": "/media/talksCardBack.13780cdd.svg"
   },
   {
     "revision": "6fa5e067005c44b331d232dc1e10e55b",
-    "url": "/static/media/udacity-logo.6fa5e067.webp"
+    "url": "/media/udacity-logo.6fa5e067.webp"
   }
 ]);
